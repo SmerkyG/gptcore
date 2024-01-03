c7eb4934dc3445a7e8f5d999b9076a68bcb3458c
SmerkyG <SmerkyG@users.noreply.github.com>
2024-01-03 14:15:51 -0800

    trainable gptab

--- model/experimental/gptalpha_based.py
+++ model/experimental/gptalpha_based.py
@@ -13,7 +13,7 @@ def taylor_exp(x: Tensor):
     x2 = (x.unsqueeze(-1) * x.unsqueeze(-2)).flatten(-2) / math.sqrt(2)
     return torch.cat([ones, x, x2], dim=-1)
 
-def gptAB_recurrent(q_in, k_in, v_in, w, kv_state, eps=1e-12):
+def gptAB_recurrent(q_in, k_in, v_in, w, kv_state = None, eps=1e-12):
     K = k_in.size(-1)
     V = v_in.size(-1)
     if kv_state is None:
@@ -29,7 +29,7 @@ def gptAB_recurrent(q_in, k_in, v_in, w, kv_state, eps=1e-12):
 
     return out, kv_state
 
-def gptAB_parallel(q, k, v, w, kv_state, eps=1e-12):
+def gptAB_parallel(q, k, v, w, kv_state = None, eps=1e-12):
     K = k.size(-1)
     V = v.size(-1)
     if kv_state is None:
@@ -75,3 +75,41 @@ def sanity_check():
 if __name__ == "__main__":
     sanity_check()
     exit()
+
+
+import model.interface
+import model.core
+
+class ApproximatedDecayedAttention(model.core.TransformerLayerPart, model.core.IAttention):
+    def __init__(self):
+        super().__init__()
+        hparams = self.hparams
+        H = hparams.n_head
+
+        self.eps = 1e-12
+
+        layer_id = self.layer_id
+        with torch.no_grad():
+            ratio_0_to_1 = layer_id / max(hparams.n_layer - 1, 1)  # 0 to 1
+            ddd = torch.ones(1, 1, hparams.d_model)
+            for i in range(hparams.d_model):
+                ddd[0, 0, i] = i / hparams.d_model
+
+            # fancy time_decay
+            decay_speed = torch.ones(H)
+            for h in range(H):
+                decay_speed[h] = -8 + 7 * (h / max(H - 1, 1)) ** (0.7 + 1.3 * ratio_0_to_1)
+            self.time_decay = nn.Parameter(decay_speed)
+
+    def forward(self, q:Tensor, k:Tensor, v:Tensor, recurrent_memory : Optional[Tensor] = None):
+        B,H,T,Q = q.shape
+
+        hparams = self.hparams
+        H = hparams.n_head
+
+        w = torch.exp(-torch.exp(self.time_decay.float())).reshape(1,H,1,1)
+
+        out, s = gptAB_parallel(q, k, v, w)
+        return out
+
+
